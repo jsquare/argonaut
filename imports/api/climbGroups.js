5e43af3c5cb36399ff5343918e01c3b43d52faba
Jaime <jaime.m.mccandless@gmail.com>
2016-10-09 17:42:48 -0700

    lint climbGroups

--- imports/api/climbGroups.js
+++ imports/api/climbGroups.js
@@ -1,69 +1,69 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
+import {Meteor} from 'meteor/meteor';
+import {Mongo} from 'meteor/mongo';
+import {check} from 'meteor/check';
 
 export const ClimbGroups = new Mongo.Collection('climbGroups');
 
 if (Meteor.isServer) {
-  // This code only runs on the server
-  // Only publish climbGroups that are public or belong to the current user
-  Meteor.publish('climbGroups', function climbGroupsPublication() {
-    return ClimbGroups.find({climber: this.userId});
-  });
+    // This code only runs on the server
+    // Only publish climbGroups that are public or belong to the current user
+    Meteor.publish('climbGroups', function climbGroupsPublication() {
+        return ClimbGroups.find({climber: this.userId});
+    });
 }
 
 Meteor.methods({
-  'climbGroups.addClimb'(routeGroupId, maxClimbs) {
-    check(routeGroupId, String);
-    check(maxClimbs, Number);
+    'climbGroups.addClimb'(routeGroupId, maxClimbs) {
+        check(routeGroupId, String);
+        check(maxClimbs, Number);
 
-    // Make sure the user is logged in before inserting a climbGroup
-    if (! this.userId) {
-      throw new Meteor.Error('not-authorized');
-    }
+        // Make sure the user is logged in before inserting a climbGroup
+        if (! this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
 
-    const climbGroup = ClimbGroups.findOne(
-      {
-        routeGroup: routeGroupId,
-        climber: this.userId
-      }
-    );
+        const climbGroup = ClimbGroups.findOne(
+            {
+                routeGroup: routeGroupId,
+                climber: this.userId,
+            }
+        );
 
-    if (climbGroup) {
-      ClimbGroups.update(
-        climbGroup._id,
-        // Would like to use $max and $inc here, but $max isn't available in MiniMongo
-        {$set: {count: Math.min(climbGroup.count + 1, maxClimbs)}}
-      );
-    } else {
-      ClimbGroups.insert({
-        climber: this.userId,
-        routeGroup: routeGroupId,
-        count: 1,
-      });
-    };
-  },
-  'climbGroups.removeClimb'(routeGroupId) {
-    check(routeGroupId, String);
+        if (climbGroup) {
+            ClimbGroups.update(
+                climbGroup._id,
+                // Would like to use $max and $inc here, but $max isn't available in MiniMongo
+                {$set: {count: Math.min(climbGroup.count + 1, maxClimbs)}}
+            );
+        } else {
+            ClimbGroups.insert({
+                climber: this.userId,
+                routeGroup: routeGroupId,
+                count: 1,
+            });
+        }
+    },
+    'climbGroups.removeClimb'(routeGroupId) {
+        check(routeGroupId, String);
 
-    // Make sure the user is logged in before inserting a climbGroup
-    if (! this.userId) {
-      throw new Meteor.Error('not-authorized');
-    }
+        // Make sure the user is logged in before inserting a climbGroup
+        if (! this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
 
-    const climbGroup = ClimbGroups.findOne(
-      {
-        routeGroup: routeGroupId,
-        climber: this.userId
-      }
-    );
+        const climbGroup = ClimbGroups.findOne(
+            {
+                routeGroup: routeGroupId,
+                climber: this.userId,
+            }
+        );
 
-    if (climbGroup) {
-      ClimbGroups.update(
-        climbGroup._id,
-        // Would like to use $min and $inc here, but $min isn't available in MiniMongo
-        {$set: {count: Math.max(climbGroup.count - 1, 0)}}
-      );
-    };
-  },
+        if (climbGroup) {
+            ClimbGroups.update(
+                climbGroup._id,
+                // Would like to use $min and $inc here, but $min isn't available in MiniMongo
+                {$set: {count: Math.max(climbGroup.count - 1, 0)}}
+            );
+        }
+    },
 });
